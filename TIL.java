4f168ec267914f55bb1c29159771b29fe0b48611
minkangpark <minkangpark@gmail.com>
2021-03-08 06:55:58 +0900

    upload

---
+++ TIL.java
@@ -0,0 +1,22 @@
+public class TIL {
+    public static void main(String[] args){
+
+        int i = 5, j = 5;
+        System.out.println(i++);
+        System.out.println(++j);
+        System.out.println("i =" + i + ", j = " +j);
+
+
+
+        /*
+        int i = 5, j = 0;
+        j = i++;
+        System.out.println("j = i++; 실행 후, i = "+i+" , j = "+j);
+
+        i = 5;
+        j = 0;
+
+        j = ++i;
+        System.out.println("j = ++i; 실행 후, i = "+i+" j = "+ j);*/
+    }
+}
